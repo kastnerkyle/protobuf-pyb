7a440ab1cc33bb5de9f46e183b72509f021ff7f1
Andy Chu <andy@chubot.org>
2012-05-27 02:45:38 -0700

    Copy some code into pyb2

--- pyb/pyb2.py
+++ pyb/pyb2.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 """
 pyb2.py
+
+Better version of pyb where we:
+
+  - Don't care about the "internal Python" interface -- we use dicts; we don't
+    use metaclasses to generate Python classes for each message type.
+  - Compile the schema into a tree of decoding functions.
+  - Lazily decode?  I don't see a need for this now, but it might fall out.
+
+
+Walk through the messageo.
+
+FieldDecoder
+  IntDecoder, etc.
+MessageDecoder
+  It contains a DICT of field decoders!
+  { tag -> decoder }
+
+  Then the decoding loop just walks through and calls these functions.
+
 """
 
 __author__ = 'Andy Chu'
@@ -18,6 +37,124 @@ class Error(Exception):
   pass
 
 
+class Message(object):
+  """A record-like object with an associated schema.
+
+  MakeMessageType generates subclasses of this class at runtime.
+  """
+
+  _field_names = set()  # A set of names, for quick lookup
+  _fields = {}  # Details about the fields, including types
+  _fields_by_tag = {}  # Index of fields by tag number
+  _repeated = set()
+
+
+
+def MakeEnums(enums, message_type):
+  """Given the enum type information, attach it to the Message class."""
+  # Like proto2, we ignore the enum type name.  All enums values live in the
+  # parent namespace.  This prevents the annoyance of long chains of dotted
+  # names.
+  for enum_type in enums:
+    for value in enum_type['value']:
+      setattr(message_type, value['name'], value['number'])
+
+
+def MakeMessageType(type_name, fields):
+  """Given the message type information, return a new subclass of Message."""
+
+  class_attrs = {}
+
+  repeated = set()  # TODO: Benchmark this
+  fields_by_tag = {}
+
+  for field in fields:
+    fields_by_tag[field['number']] = field
+
+    # Repeated fields are empty by default.
+    if field['label'] == 'LABEL_REPEATED':
+      repeated.add(field['name'])
+
+    # Fill in defaults as class attributes.  This takes advantage of Python's
+    # normal attribute lookup order.
+    if 'default_value' in field:
+      class_attrs[field['name']] = field['default_value']
+
+  class_attrs.update({
+      '_field_names': set(f['name'] for f in fields),
+      '_fields': fields,
+      '_fields_by_tag': fields_by_tag,
+      '_repeated': repeated,  # repeated fields
+      })
+
+  return type(str(type_name), (Message,), class_attrs)
+      
+
+def MakeMessageTypes(messages, package, name_list, root, type_index):
+  """Create a hierarchy of message types, given information from the
+  DescriptorSet.
+  
+  Args:
+    messages: A list of dictionaries.  Each should have a 'name' and a list of
+       fields.
+    package: The proto package.  This is generally irrelevant to the user of the
+        API, but still used internally.
+    name_list: Stack of names
+    root: The type that encloses these type, or the root _ProtoPackage
+    type_index: The message types are registered in this "global" index of types
+  """
+  for message_data in messages:
+    type_name = message_data['name']
+    names = name_list + [type_name]  # NOTE: Creating a new list!
+    full_name = '.'.join(names)
+
+    fields = message_data['field']
+    message_type = MakeMessageType(full_name, fields) 
+    # Attach the subtype to the root type
+    setattr(root, type_name, message_type)
+    # Populate the type index owned by an instance of DescriptorSet
+    key = '.%s.%s' % (package, full_name)
+    type_index[key] = message_type
+    # Every message type needs the type index, in order to decode its subtypes.
+    # (We could technically make an index for each type containing its subtypes,
+    # but it's simpler and cheaper to just use a 'global' one everywhere)
+    message_type.type_index = type_index
+
+    subtypes = message_data.get('nested_type', [])
+    # Recursive call with 'message_type' as the new root
+    MakeMessageTypes(subtypes, package, names, message_type, type_index)
+    MakeEnums(message_data.get('enum_type', []), message_type)
+
+
+class _ProtoPackage(object):
+  """Used to create protobuf namespaces."""
+
+
+def MakeTypes(descriptor_set, type_index):
+  """Make a tree of Message types.
+
+  Args:
+    descriptor_set: A JSON-like dictionary representation of the
+       FileDescriptorSet.
+    type_index: A type index to populate
+
+  Returns:
+    A _ProtoPackage instance
+  """
+  # Flatten the file structure, and look up types by their fully qualified
+  # names
+  root = _ProtoPackage()
+
+  # TODO: reflect the directory structure of 'file'
+  for f in descriptor_set['file']:
+    package = f.get('package', '')
+    MakeMessageTypes(f['message_type'], package, [], root, type_index)
+    MakeEnums(f.get('enum_type', []), root)
+
+  #print.pprint(pool.keys())
+  return root
+
+
 class AttrDict(dict):
   def __getattr__(self, attr):
     """Called when the attribute doesn't exist "statically". """
@@ -27,13 +164,30 @@ class AttrDict(dict):
       raise AttributeError
 
 
-
 def _LoadDescriptorProto():
-  f = open('descriptor.proto.json')
+  f = open('data/descriptor.proto.json')
   d = json.load(f, object_hook=AttrDict)
   f.close()
   return d
 
+
 descriptor_proto = _LoadDescriptorProto()
 print type(descriptor_proto)
 print descriptor_proto.keys()
+
+
+def main(argv):
+  type_index = {}
+  f = open('testdata/addressbook/addressbook.desc.json-from-protoc')
+  d = json.load(f)
+  MakeTypes(d, type_index)
+  print 'INDEX', type_index
+
+
+if __name__ == '__main__':
+  try:
+    success = main(sys.argv)
+    sys.exit(not success)
+  except Error, e:
+    print >> sys.stderr, e.args[0]
+    sys.exit(1)
