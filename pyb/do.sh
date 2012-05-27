1e3b4e073a11f0505d0c31024f63009f5d50bd76
Andy Chu <Andy Chu@localhost>
2012-05-26 23:17:49 -0700

    Testing out case sensitivity of existing proto compiler (for PGI)

--- pyb/do.sh
+++ pyb/do.sh
@@ -9,6 +9,21 @@ build-cpp() {
   tree cpp
 }
 
+# damnit, this fails on ubuntu
+build-py() {
+  set -o errexit
+  mkdir -p py
+  protoc --py_out py "$@"
+  tree py
+}
+
+build-java() {
+  set -o errexit
+  mkdir -p java
+  protoc --java_out java "$@"
+  tree java
+}
+
 addr() {
   build-cpp testdata/addressbook/addressbook.proto
 }
